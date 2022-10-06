c27e1289b866c37f9fa59bfaf1d8b0e5a65b3d2e
Egor Svitov <svitov.ev@phystech.edu>
2022-10-06 13:23:30 +0300

    3

--- 4-1-dac.py
+++ 4-1-dac.py
@@ -1,24 +1,34 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-def decimal2binary(value):
- 
-    return [int(bit) for bit in bin(value)[2:].zfill(8)]
+import time
+
 dac = (26, 19, 13, 6, 5, 11, 9, 10)
-for count in dac:
-    GPIO.setup(count, GPIO.OUT) 
-inp_number = 0
+comp = 4
+troyka = 17
+def decimal2binary(value): 
+    return [int(bit) for bit in bin(value)[2:].zfill(8)]
+GPIO.setmode(GPIO.BCM)
+for count in range(0,8):
+    GPIO.setup(dac[count], GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT, initial = 1)
+GPIO.setup(comp, GPIO.IN)
+
 try:
-    while(-1 < inp_number < 256):
-        inp_number = (int(input()))
-        print("напряжение:", 3.3*inp_number/255)
-        number_bin = decimal2binary((inp_number))
-        for count in range(0,8):
-            if number_bin[count] == 1:
-                GPIO.output(dac[count], 1)
-            else:
-                GPIO.output(dac[count], 0)
-except:
-    print("quit")
+    while(1):
+        GPIO.output(17, 1)
+        inp = 0
+        for i in range(8,0, -1):
+            time.sleep(0.001)
+            GPIO.output(dac, decimal2binary(inp + 2**i))
+            if GPIO.input(4) == 1:
+                inp = inp + 2**i
+        voltage = inp/(2**8)*3.3
+        print(voltage)
+
+except KeyboardInterrupt:
+    print('stop')
+else:
+    print('asd')
 finally:
-    for count in range(0,8):
-        GPIO.output(dac[count], 0)
+    GPIO.output(dac, 0)
+    GPIO.output(4, 0)
+    GPIO.cleanup(dac)
