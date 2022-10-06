69fdcb190a33593e2a136ed4b135af1d9d588068
Egor Svitov <svitov.ev@phystech.edu>
2022-10-06 13:31:31 +0300

    123

--- 1.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.OUT)
-for count in range(1, 10):
-    GPIO.output(23, 1)
-    time.sleep(1)
-    GPIO.output(23, 0)
-    time.sleep(1)
--- 2.py
+++
@@ -1,6 +0,0 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.OUT)
-GPIO.setup(23, GPIO.IN)
-if True:
-    GPIO.output(24, GPIO.input(23))
